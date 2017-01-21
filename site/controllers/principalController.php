754bd50efa672d62ca441776a16fadbb2d77cc0f
Rafael Castañeda <Rafael Castaeda>
2017-01-21 17:33:06 -0400

    Vista principal con collage de imagenes para mostrar a las niñas

--- site/controllers/principalController.php
+++ site/controllers/principalController.php
@@ -24,8 +24,8 @@ class principalController extends Controller
 		
        
 			
-			$this->_view->setJs(array('index'));
-			$this->_view->setCss(array('css'));
+			$this->_view->setJs(array('index','jquery.montage'));
+			$this->_view->setCss(array('css','style'));
         		$this->_view->titulo = 'index';
 			$this->_view->renderizar('index');
 							
