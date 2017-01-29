74c71b0d06c1d7c00a7e791fbed9fbee606caf09
alz92 <adellemusb@hotmail.com>
2017-01-29 13:22:17 -0400

    cambios a la vista niñas

--- site/views/nina/js/index.js
+++ site/views/nina/js/index.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
-
+id=1;
 
 $(document).on("click", "#prevista > img", function(){
 	id=this.id;
+	id_foto=$("#"+id).attr('src');
+	var options = {};
+	$( "#fotoG" ).hide( "fade", options, 1000, callback(id_foto));
+	
+	
+})
+
+$(document).on("click", "#mover", function(){
+	mover=$(this).data('mover');
+	console.log(id);
+	if(mover=='derecha'){
+			id=id+1;
+			if(id>6){
+				id=1;
+			}
+		}
+		else{
+			id=id-1;
+			if(id<1){
+				id=6;
+			}
+		}
+	
 	id_foto=$("#"+id).attr('src');
 	var options = {};
 	$( "#fotoG" ).hide( "fade", options, 1000, callback(id_foto));
